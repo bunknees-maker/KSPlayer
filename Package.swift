78202fe4dcf0f7966a0635b72c0425795742fce6
bunknees-maker <bunknees@gmail.com>
2025-11-02 14:15:54 -0500

    KSPlayer: pin FFmpegKit to bunknees-maker/FFmpegKit exact 6.1.4 (drops -D_THREAD_SAFE)

--- Package.swift
+++ Package.swift
@@ -61,6 +61,6 @@ if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     ]
 } else {
     package.dependencies += [
-        .package(url: "https://github.com/bunknees-maker/FFmpegKit.git", branch: "cz/threadsafe-fix"),
+        .package(url: "https://github.com/bunknees-maker/FFmpegKit.git", exact: "6.1.4"),
     ]
 }
