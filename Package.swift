995b7e5928db12adc8c1996a2e4ddffca898382e
bunknees-maker <bunknees@gmail.com>
2025-11-02 14:31:45 -0500

    KSPlayer: use static FFmpegKit dependency (from 6.1.4) - removes conditional logic for xcodebuild compatibility

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
             targets: ["KSPlayer"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/bunknees-maker/FFmpegKit.git", from: "6.1.4"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         .target(
@@ -41,26 +44,3 @@ let package = Package(
         ),
     ]
 )
-
-var ffmpegKitPath = FileManager.default.currentDirectoryPath + "/FFmpegKit"
-if !FileManager.default.fileExists(atPath: ffmpegKitPath) {
-    ffmpegKitPath = FileManager.default.currentDirectoryPath + "/../FFmpegKit"
-}
-
-if !FileManager.default.fileExists(atPath: ffmpegKitPath), let url = URL(string: #file) {
-    let path = url.deletingLastPathComponent().path
-    // 解决用xcode引入spm的时候，依赖关系出错的问题
-    if !path.contains("/checkouts/") {
-        ffmpegKitPath = path + "/../FFmpegKit"
-    }
-}
-
-if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
-    package.dependencies += [
-        .package(path: ffmpegKitPath),
-    ]
-} else {
-    package.dependencies += [
-        .package(url: "https://github.com/bunknees-maker/FFmpegKit.git", from: "6.1.4"),
-    ]
-}
