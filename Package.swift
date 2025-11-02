732fa400da8dbc8286f148e5b9d3890e70f633cb
bunknees-maker <bunknees@gmail.com>
2025-11-02 14:26:35 -0500

    KSPlayer: change FFmpegKit dependency from exact to from 6.1.4

--- Package.swift
+++ Package.swift
@@ -61,6 +61,6 @@ if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     ]
 } else {
     package.dependencies += [
-        .package(url: "https://github.com/bunknees-maker/FFmpegKit.git", exact: "6.1.4"),
+        .package(url: "https://github.com/bunknees-maker/FFmpegKit.git", from: "6.1.4"),
     ]
 }
